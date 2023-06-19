d53ef65c70618ce8afd6b74ec33876573a8f3955
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-06-19 21:56:53 +0400

    director update query added

--- src/components/DirectorsForm/DirectorsFormHoc.js
+++ src/components/DirectorsForm/DirectorsFormHoc.js
@@ -2,7 +2,7 @@ import { withStyles } from '@mui/styles';
 import { compose } from 'recompose';
 import { graphql } from '@apollo/client/react/hoc';
 
-import { addDirectorMutation } from './mutations';
+import { addDirectorMutation, updateDirectorMutation } from './mutations';
 import {directorsQuery} from '../DirectorsTable/queries'
 
 import { styles } from './styles';
@@ -15,5 +15,13 @@ const withGraphqlAdd = graphql(addDirectorMutation, {
         })
     })
 })
+const withGraphqlUpdate = graphql(updateDirectorMutation, {
+    props: ({mutate}) => ({
+        updateDirector: director => mutate({
+            variables: director,
+            refetchQueries: [{query: directorsQuery}]           //For reload data
+        })
+    })
+})
 
-export default compose(withStyles(styles), withGraphqlAdd);
+export default compose(withStyles(styles), withGraphqlAdd, withGraphqlUpdate);
