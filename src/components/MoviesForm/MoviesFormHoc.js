ba369c97c1ed80e28e5ecc1b0308a94cb7e60d56
DmitriyKanischev <116942865+DmitriyKanischev@users.noreply.github.com>
2023-06-20 07:23:03 +0400

    movies update query added

--- src/components/MoviesForm/MoviesFormHoc.js
+++ src/components/MoviesForm/MoviesFormHoc.js
@@ -2,7 +2,7 @@ import { withStyles } from '@mui/styles';
 import { compose } from 'recompose';
 import { graphql } from '@apollo/client/react/hoc';
 
-import { addMovieMutation } from './mutations';
+import { addMovieMutation, updateMovieMutation } from './mutations';
 import { moviesQuery } from '../MoviesTable/queries';  //For refetch movies data
 import { directorsQuery } from './queries';
 
@@ -16,5 +16,13 @@ const withGraphqlAdd = graphql(addMovieMutation, {
         })
     })
 })
+const withGraphqlUpdate = graphql(updateMovieMutation, {
+    props: ({mutate}) => ({
+        updateMovie: movie => mutate({
+            variables: movie,
+            refetchQueries: [{query: moviesQuery}]
+        })
+    })
+})
 
-export default compose(withStyles(styles), withGraphqlAdd, graphql(directorsQuery));
+export default compose(withStyles(styles), withGraphqlAdd, withGraphqlUpdate, graphql(directorsQuery));
--- src/components/MoviesForm/mutations.js
+++ src/components/MoviesForm/mutations.js
@@ -6,4 +6,11 @@ export const addMovieMutation = gql`
             name
         }
     }
+`;
+export const updateMovieMutation = gql`
+    mutation updateMovie($id: ID, $name: String!, $genre: String!, $watched: Boolean!, $rate: Int, $directorId: ID){
+        updateMovie(id: $id, name: $name, genre: $genre, watched: $watched, rate: $rate, directorId: $directorId){
+            name
+        }
+    }
 `
